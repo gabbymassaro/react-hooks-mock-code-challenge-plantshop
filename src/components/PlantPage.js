d3a4c18393449e83c4e7fff14c1dc1fe127c7f4b
Gabriella Massaro <gmassaro807@gmail.com>
2024-06-27 19:56:36 -0400

    Render all plants on page load and toggle stock

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,16 +1,27 @@
-import React from "react";
-import NewPlantForm from "./NewPlantForm";
-import PlantList from "./PlantList";
-import Search from "./Search";
+import React, { useEffect, useState } from "react"
+import NewPlantForm from "./NewPlantForm"
+import PlantList from "./PlantList"
+import Search from "./Search"
 
 function PlantPage() {
+  const [plants, setPlants] = useState([])
+  const [fetchTrigger, setFetchTrigger] = useState(false)
+
+  const toggleFetchTrigger = () => setFetchTrigger(!fetchTrigger)
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((response) => response.json())
+      .then((data) => setPlants(data))
+  }, [fetchTrigger])
+
   return (
     <main>
       <NewPlantForm />
       <Search />
-      <PlantList />
+      <PlantList plants={plants} />
     </main>
-  );
+  )
 }
 
-export default PlantPage;
+export default PlantPage
