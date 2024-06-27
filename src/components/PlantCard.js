ca9a3c13984acfebc64bf8d0ec365f98a92e5f48
Gabriella Massaro <gmassaro807@gmail.com>
2024-06-27 08:52:05 -0400

    Create GET and render all plants on page load

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,18 +1,20 @@
-import React from "react";
+import React from "react"
 
-function PlantCard() {
+function PlantCard({ plant }) {
+  const { id, name, image, price } = plant
+
   return (
     <li className="card">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      <p>Price: {price}</p>
       {true ? (
         <button className="primary">In Stock</button>
       ) : (
         <button>Out of Stock</button>
       )}
     </li>
-  );
+  )
 }
 
-export default PlantCard;
+export default PlantCard
